6ef02a44a9b879f218a9a70f2e952676bdfebe05
Markiian <mordecryt@gmail.com>
2024-10-22 20:01:59 +0300

    initial commit

---
+++ lab5/Controllers/LineController.cs
@@ -0,0 +1,20 @@
+﻿using lab5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab5.Controllers
+{
+    public class LineController : Controller
+    {
+        [HttpGet]
+        public IActionResult DrawLine()
+        {
+            return View(new LineAttributes());
+        }
+
+        [HttpPost]
+        public IActionResult DrawLine(LineAttributes model)
+        {
+            return View(model);  // Повертаємо модель з атрибутами лінії для відображення
+        }
+    }
+}
---
+++ lab5/Models/LineAttributes.cs
@@ -0,0 +1,11 @@
+﻿namespace lab5.Models;
+
+public class LineAttributes
+{
+    public int Thickness { get; set; }
+    public string Color { get; set; }
+    public int StartX { get; set; }
+    public int StartY { get; set; }
+    public int EndX { get; set; }
+    public int EndY { get; set; }
+}
